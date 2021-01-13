7a685fd2674b2fbdd08fec79c7359df18d1c7820
Akito-n <a.nagai@mof-mof.co.jp>
2021-01-13 23:53:43 +0900

    generate auth module

--- src/modules/book.module.ts
+++ src/modules/book.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookResolver } from 'src/resolvers/book/book.resolver';
 import { BookService } from 'src/services/book/book.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   providers: [BookResolver, BookService]
 })
 export class BookModule {}
